23b12132254227a840e1b35f32a42fbf0e9c0669
jeremy <jeraguilon@gmail.com>
2016-10-10 03:26:29 -0400

    m6: Skeleton added for security logger tester

--- src/tests/logging/SecurityLoggerTest.java
+++ src/tests/logging/SecurityLoggerTest.java
@@ -56,4 +56,10 @@ public class SecurityLoggerTest {
         assertTrue(file.exists());
     }
 
+    @Test
+    public void secondEntryOnDate_ShouldNotOverwriteTheFile() {}
+
+    @Test
+    public void entriedData_ShouldMatchExpectedFormat() {}
+
 }
