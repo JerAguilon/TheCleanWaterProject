7d08f50d81617b47f612844f9e79bb0df82d9517
Ashima Gauba <ashima11gauba96@gmail.com>
2016-10-26 15:30:51 -0400

    Javadocs Updated

--- src/model/Report.java
+++ src/model/Report.java
@@ -56,44 +56,85 @@ public abstract class Report implements Loggable {
         this.idColumnProperty.set(ID);
     }
 
+    /**
+     * gets the date and time
+     * @return the LocalDateTime
+     */
     public LocalDateTime getDateTime() {
         return dateTime;
     }
 
+    /**
+     * gets the DateColumnProperty String value
+     * @return the dateColumnProperty String value
+     */
     public String getDateColumn() {
         return dateColumnProperty.get();
     }
 
+    /**
+     * sets the date and time to a given dateTime
+     * @param dateTime the date and time you would like to set it to
+     */
     public void setDateColumn(LocalDateTime dateTime) {
         this.dateTime = dateTime;
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
         dateColumnProperty.set(dateTime.format(formatter));
     }
 
+    /**
+     * gets the IdColumnProperty String value
+     * @return the IdColumn String value
+     */
     public String getIdColumn() {
         return idColumnProperty.get();
     }
 
+    /**
+     * sets the report ID to a given reportID
+     * @param reportID the reportID you would like to set it to as a String
+     */
     public void setIdColumn(String reportID) {
         this.idColumnProperty.set(reportID);
     }
 
+    /**
+     * gets the UsernameColumnProperty String value
+     * @return the UsernameColumn String value
+     */
     public String getUsernameColumn() {
         return this.usernameColumnProperty.get();
     }
 
+    /**
+     * sets the name of the reporter
+     * @param reporterName the name of the reporter as a String
+     */
     public void setUsernameColumn(String reporterName) {
         this.usernameColumnProperty.set(reporterName);
     }
 
+    /**
+     * gets the LocationColumnProperty String value
+     * @return the LocationColumn String value
+     */
     public String getLocationColumn() {
         return locationColumnProperty.get();
     }
 
+    /**
+     * sets the location of the reporter
+     * @param location the location of the reporter
+     */
     public void setLocationColumn(String location) {
         locationColumnProperty.set(location);
     }
 
+    /**
+     * checks if two objects are equal to one another
+     * @param other the Object that we are comparing the calling object to
+     * @return true if the objects are equal and false if they are not
+     */
     public boolean equals(Object other) {
         if (!(other instanceof Report)) {
             return false;
@@ -106,10 +147,18 @@ public abstract class Report implements Loggable {
         return false;
     }
 
+    /**
+     * calculates the hashcode
+     * @return the hashcode as an int
+     */
     public int hashCode() {
         return this.idColumnProperty.get().hashCode();
     }
 
+    /**
+     * logs the report
+     * @return a List of Strings of the log
+     */
     public List<String> toLogList() {
         List<String> output = new ArrayList<>();
 
